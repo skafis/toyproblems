41320d1c7d89172bdf0bda5dea7a6f49de1230c0
skafis <franciskipchumba5@gmail.com>
2016-02-15 08:47:39 +0300

    toy problems

---
+++ remove_duplicates/src/removeDuplicates.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+
+public class removeDuplicates {
+	public static void main (String[] args){
+		String str = "tree traversals";
+		HashMap <Integer, Character>charStore = new HashMap<Integer, Character>(); //creating a hash map to store the char
+		char [] charArray= full.toCharArray();
+		for (Character c: charArray ){
+			if (!charstore.contaainsValue(c))//if the char is not already in the hashmap
+            {
+				remove=remove+c.toString();
+				charStore.put(c.hashCode (),c }
+		}
+	}
+	}
+}
